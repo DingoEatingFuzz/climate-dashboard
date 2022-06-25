8c45318faf6980955ea001cd47821284c15b7c30
Michael Lange <dingoeatingfuzz@gmail.com>
2022-06-25 14:05:36 -0700

    Test page for the DB package
    The DB package is meant to be consumed by the reference
    implementations, but having this page helps quickly see how the DB
    package works and what data is in the database.

---
+++ db/test.js
@@ -0,0 +1,91 @@
+import DB from './index';
+
+document.addEventListener('DOMContentLoaded', main);
+
+async function main() {
+  const db = new DB();
+  const app = document.querySelector('#app');
+
+  console.time('init');
+  await db.init();
+  console.timeEnd('init');
+
+  app.append(
+    heading("Tables"),
+    table(await db.describe()),
+  );
+  app.append(
+    heading("Table: weather"),
+    table(await db.describe('weather')),
+  );
+  app.append(
+    heading("Table: stations"),
+    table(await db.describe('stations')),
+  );
+  app.append(
+    heading("Sample: stations"),
+    table(await db.query('select * from stations limit 10')),
+  );
+  app.append(
+    heading("Sample: weather"),
+    table(await db.query('select * from weather limit 10')),
+  );
+
+  const query = `
+SELECT
+  s.name, w.year, w.month, concat(round(avg(w.value)/10, 2), '° C')
+FROM stations s
+LEFT JOIN weather w ON s.id = w.id
+WHERE
+  w.year = 2021 AND w.element = 'TAVG' AND w.value != -9999
+GROUP BY
+  s.name, w.year, w.month
+ORDER BY
+  s.name, w.month
+LIMIT 500;
+  `;
+
+  app.append(
+    heading("Sample: join"),
+    snippet(query),
+    table(await db.query(query)),
+  );
+}
+
+function heading(str, level = 'h2') {
+  const heading = document.createElement(level);
+  heading.append(str);
+  return heading;
+}
+
+function snippet(str) {
+  const pre = document.createElement('pre');
+  pre.append(str.trim());
+  return pre;
+}
+
+function table(res) {
+  const fragment = new DocumentFragment();
+  const table = document.createElement('table');
+  const head = document.createElement('tr');
+  res.columns.forEach(col => {
+    const cell = document.createElement('td');
+    cell.append(col);
+    head.append(cell);
+  });
+
+  const rows = res.rows.map(row => {
+    const tr = document.createElement('tr');
+    const cells = res.columns.map(col => {
+      const cell = document.createElement('td');
+      cell.append(row[col]);
+      return cell;
+    });
+    tr.append(...cells);
+    return tr;
+  });
+
+  table.append(head, ...rows);
+  fragment.append(table);
+  return fragment;
+}
