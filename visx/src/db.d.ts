cc899cb9e7f37cd72386dce765a2cd68a842f5a9
Michael Lange <dingoeatingfuzz@gmail.com>
2022-07-07 09:42:22 -0700

    Introduce DB + types for DB

---
+++ visx/src/db.d.ts
@@ -0,0 +1,39 @@
+declare class Station {
+  id: string;
+  lat: Number;
+  lon: Number;
+  elevation: Number;
+  state: string;
+  name: string;
+  gsn: boolean;
+  hcn: boolean;
+  crn: boolean;
+  wmo: Number;
+  sampled: boolean;
+}
+
+declare type DuckResult<Type> = {
+  columns: string[],
+  rows: Array<Type>,
+}
+
+declare class DB {
+  constructor();
+
+  public ready: boolean;
+  public init(): Promise;
+
+  // Connection is technically public, but it shouldn't be used directly, so it's left untyped
+
+  public query(query: string, ...params:Array<any>): Promise<DuckResult>;
+  public describe(table:string|undefined): Promise<DuckResult>;
+  public stations(): Promise<DuckResult<Station>>;
+  public monthlyAverageForStation(station:Station|string): Promise<DuckResult>;
+  public weatherForStationForRange(station:Station|string, start:Date, end:Date): Promise<DuckResult>;
+  public averagesForStation(station:Station|string): Promise<DuckResult>;
+  public weatherDetailByMonth(station:Station|string): Promise<DuckResult>;
+}
+
+declare module 'db' {
+  export = DB;
+};
