8008e47a71e2615da7a7e5267315922b358818a3
Michael Lange <dingoeatingfuzz@gmail.com>
2022-07-07 09:42:47 -0700

    First DB layer interaction

--- visx/src/App.tsx
+++ visx/src/App.tsx
@@ -1,16 +1,38 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import DB from 'db';
+
+const db = new DB();
 
 function App() {
-  const [station, setStation] = useState('')
+  const [station, setStation] = useState<Station | undefined>(undefined)
+  const [stations, setStations] = useState<DuckResult<Station> | undefined>(undefined)
+
+  const stationForId = (id: string):Station|undefined => {
+    return stations?.rows.find(s => s.id === id);
+  }
+
+  useEffect(() => {
+    const dbCall = async () => {
+      if (!db.ready) await db.init();
+      setStations(await db.stations());
+      if (!station) {
+        setStation(stations?.rows.filter(s => s.sampled)[0]);
+      }
+    };
+
+    dbCall().catch(console.error);
+  }, []);
 
   return (
     <div className="App">
       <div className="Header">
-        <h1>{station}</h1>
-        <select>
-          <option>Placeholder</option>
+        <h1>{station?.name}</h1>
+        <select onChange={e => setStation(stationForId(e.target.value))}>
+          {stations && stations.rows.filter(s => s.sampled).map(s => (
+            <option key={s.id} value={s.id}>{s.name}</option>
+          ))}
         </select>
       </div>
       {/* <TimeSeries station={station} start={start} end={end} /> */}
